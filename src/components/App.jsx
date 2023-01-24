86189ae6904c38dae5165e833728ecea22d89b0a
Amankumar Churiwal <amanchuriwal22@gmail.com>
2023-01-24 16:53:41 +0530

    added notes json

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,12 +2,33 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
+import notes from "../notes"
+
+/*
+let createNote = (noteItem) => {
+    return (
+        <Note 
+        key={noteItem.key}
+        title={noteItem.title}
+        content={noteItem.content}
+        />
+    )
+}
+*/
 
 let App = () => {
     return (
         <div>
             <Header />
-            <Note />
+            {notes.map((noteItem) => {
+                return (
+                    <Note 
+                    key={noteItem.key}
+                    title={noteItem.title}
+                    content={noteItem.content}
+                    />
+                )
+            })}
             <Footer />
         </div>
     )
